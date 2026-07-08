18ea86bf0f6479c86abfbe7ac46ced3cf89a0e20
divakar singh purva <divakarpurva04@gmail.com>
2026-07-09 04:01:04 +0530

    Left Section Completed

--- components/left/Cta.tsx
+++ components/left/Cta.tsx
@@ -6,22 +6,26 @@ function Cta() {
     return (
         <div className="m-8 ">
             <div className="p-8 flex flex-1 items-center justify-around">
-                <button
+                <a
+                    href="/resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="
-    group
-    flex items-center gap-2
-    rounded-xl
-    border border-dashed
-    bg-cta
-    px-5 py-3
-    transition-all duration-300
-    hover:scale-[1.02]
-    active:translate-y-0
-    active:shadow-none
-    cursor-pointer
-    relative
-  "
+                        group
+                        flex items-center gap-2
+                        rounded-xl
+                        border border-dashed
+                        bg-cta
+                        px-5 py-3
+                        transition-all duration-300
+                        hover:scale-[1.02]
+                        active:translate-y-0
+                        active:shadow-none
+                        cursor-pointer
+                        relative
+                    "
                 >
+                    {/* Noise Overlay */}
                     <div
                         className="absolute inset-0 rounded-xl pointer-events-none opacity-10"
                         style={{
@@ -32,31 +36,32 @@ function Cta() {
                     <ReadCvLogoIcon
                         size={24}
                         className="
-      transition-transform
-      duration-300
-      rotate-[-11deg]
-      group-hover:rotate-3
-    "
+                            transition-transform
+                            duration-300
+                            rotate-[-11deg]
+                            group-hover:rotate-3
+                        "
                     />
-
                     <span>View Resume / CV</span>
-                </button>
-                <button
+                </a>
+                <a
+                    href="mailto:divakarpurva04@gmail.com?subject=Hello from your Portfolio"
                     className="
-    group
-    flex items-center gap-2
-    rounded-xl
-    border border-dashed bordercta
-    bg-cta
-    px-5 py-3
-    transition-all duration-300
-    hover:scale-[1.02]
-    active:translate-y-0
-    active:shadow-none
-    cursor-pointer
-    relative
-  "
+                        group
+                        flex items-center gap-2
+                        rounded-xl
+                        border border-dashed
+                        bg-cta
+                        px-5 py-3
+                        transition-all duration-300
+                        hover:scale-[1.02]
+                        active:translate-y-0
+                        active:shadow-none
+                        cursor-pointer
+                        relative
+                    "
                 >
+                    {/* Noise Overlay */}
                     <div
                         className="absolute inset-0 rounded-xl pointer-events-none opacity-10"
                         style={{
@@ -68,15 +73,14 @@ function Cta() {
                     <PaperPlaneTiltIcon
                         size={20}
                         className="
-      transition-transform
-      duration-300
-      group-hover:translate-x-0.5
-      group-hover:-translate-y-0.5
-    "
+                            transition-transform
+                            duration-300
+                            group-hover:translate-x-0.5
+                            group-hover:-translate-y-0.5
+                        "
                     />
-
                     <span>Get in Touch</span>
-                </button>
+                </a>
             </div>
         </div>
     );
