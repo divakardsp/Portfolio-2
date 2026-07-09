af9a046dd6b2998cf6d58d94f13800ad9a17a93d
divakar singh purva <divakarpurva04@gmail.com>
2026-07-09 08:03:17 +0530

    style: update background colors for RightSection and Name components

--- components/RightSection.tsx
+++ components/RightSection.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "./right/Navbar"
 
 function RightSection() {
     return (
         <>
-            <section className="min-h-screen w-3/5 bg-main transition-colors duration-[700ms]">
-                
+            <section className="min-h-screen w-3/5 bg-main transition-colors duration-[700ms] bg-main text-muted">
+                <div className="p-4">
+                    <Navbar/>
+                </div>
             </section>
         </>
     );
--- components/left/Name.tsx
+++ components/left/Name.tsx
@@ -21,8 +21,8 @@ function Name() {
                 >
                     {/* Blinking Dot */}
                     <span className="relative flex h-2.5 w-2.5">
-                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-emerald-500"></span>
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-600 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-2.5 w-2.5  bg-emerald-700"></span>
                     </span>
                     Available for new opportunities
                 </div>
