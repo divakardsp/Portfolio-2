fd95162516a0ff781cd40a800185a09e958c306e
divakar singh purva <divakarpurva04@gmail.com>
2026-07-11 04:51:49 +0530

    feat: enhance About component with improved text styling for better readability

--- components/right/About.tsx
+++ components/right/About.tsx
@@ -7,7 +7,7 @@ function About() {
                 About Me
             </h1>
             <p className="text-lg tracking-tight leading-7">
-                Hello! Divakar here. 👋
+                Hello! <span className="text-foreground">Divakar</span> here. 👋
             </p>
             <br />
             <p className="text-lg tracking-tight leading-7">
@@ -18,19 +18,21 @@ function About() {
             <br />
             <p className="text-lg tracking-tight leading-7">
                 Even though I enjoy working on the backend the most, I like
-                building the whole product. For me, a good product isn&apos;t just
-                fast or scalable. It should be easy to use, reliable, and
+                building the whole product. For me, a good product isn&apos;t
+                just fast or scalable. It should be easy to use, reliable, and
                 something people actually enjoy using.
             </p>
             <br />
             <p className="text-lg tracking-tight leading-7">
-                These days, I&apos;m spending a lot of time exploring AI
-                applications, authentication systems, and real-time
-                architectures. I also enjoy digging into performance issues,
-                understanding how things work behind the scenes, and making them
-                faster, cleaner, and more secure. Whether it&apos;s a database query,
-                a protocol, or a small security bug, I like figuring out what&apos;s
-                happening and how to make it better.
+                These days, I&apos;m spending a lot of time exploring{" "}
+                <span className="text-foreground">AI applications</span>,{" "}
+                <span className="text-foreground">authentication systems</span>,
+                and <span className="text-foreground">real-time architectures</span>. I also enjoy digging into
+                performance issues, understanding how things work behind the
+                scenes, and making them <span className="text-foreground">faster, cleaner, and more secure. </span>
+                Whether it&apos;s a database query, a protocol, or a small
+                security bug, I like figuring out what&apos;s happening and how
+                to make it better.
             </p>
         </div>
     );
