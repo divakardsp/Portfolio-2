63e63eef5bacfe6399c95c82b57380dd6271a9f1
divakar singh purva <divakarpurva04@gmail.com>
2026-07-20 02:33:31 +0530

    feat: add introductory animation using framer-motion and update layout for Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import LeftSection from "@/components/LeftSection";
 import RightSection from "@/components/RightSection";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function Home() {
     const [showIntro, setShowIntro] = useState(true);
@@ -16,22 +17,45 @@ export default function Home() {
         return () => clearTimeout(timer);
     }, []);
 
-    if (showIntro) {
-        return (
-            <div className="flex min-h-screen items-center justify-center bg-main">
-                <DotLottieReact
-                    src="/Hello.lottie"
-                    autoplay
-                    loop={false}
-                />
-            </div>
-        );
-    }
-
     return (
-        <div className="flex flex-col lg:flex-row min-h-screen">
-            <LeftSection />
-            <RightSection />
-        </div>
+        <>
+            <AnimatePresence>
+                {showIntro ? (
+                    <motion.div
+                        key="intro"
+                        className="fixed inset-0 flex items-center justify-center bg-main"
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        <DotLottieReact
+                            src="/Hello.lottie"
+                            autoplay
+                            loop={false}
+                        />
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        className="flex flex-col lg:flex-row min-h-screen"
+                        initial={{ opacity: 0, scale: 0 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            duration: 0.3,
+                        }}
+                    >
+                        <LeftSection />
+                        <RightSection />
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
+            {/* {!showIntro && (
+                <div className="flex flex-col lg:flex-row min-h-screen">
+                    <LeftSection />
+                    <RightSection />
+                </div>
+            )} */}
+        </>
     );
 }
