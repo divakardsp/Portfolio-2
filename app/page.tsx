9b748f249ad70368e7fb63c7e08100421f0fb109
divakar singh purva <divakarpurva04@gmail.com>
2026-07-20 01:49:14 +0530

    feat: implement introductory animation with DotLottie and update layout for Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
-import Footer from "@/components/Footer";
+"use client";
+
+import { useEffect, useState } from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import LeftSection from "@/components/LeftSection";
 import RightSection from "@/components/RightSection";
-import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <div className="flex flex-col lg:flex-row min-h-screen">
-        <LeftSection/>
-        <RightSection/>
-      </div>
-    </>
-  );
+    const [showIntro, setShowIntro] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowIntro(false);
+        }, 4000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (showIntro) {
+        return (
+            <div className="flex min-h-screen items-center justify-center bg-main">
+                <DotLottieReact
+                    src="/Hello.lottie"
+                    autoplay
+                    loop={false}
+                />
+            </div>
+        );
+    }
+
+    return (
+        <div className="flex flex-col lg:flex-row min-h-screen">
+            <LeftSection />
+            <RightSection />
+        </div>
+    );
 }
